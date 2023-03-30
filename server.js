b10d2d754bf7d12964830ec7c2ce1f9ffc206dec
hoanganh.axe.no.1@gmail.com <hoanganh.axe.no.1@gmal.com>
2023-03-30 22:26:46 +0700

    delete comment & demo folder

--- server.js
+++ server.js
@@ -10,12 +10,6 @@ app.listen(config.PORT, config.HOST_NAME, () => {
     console.log(`Server is running on : ${config.HOST_NAME}:${config.PORT}`);
 })
 
-// app.all('/', function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//     next()
-// });
-
 app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
